0db3b082be34ee249cda51d453e5c2e1b05e14ed
olexiy <o.chuiko@temabit.com>
2021-04-17 14:40:59 +0300

    change vendor name

--- FozzyGraphQLBundle.php
+++ FozzyGraphQLBundle.php
@@ -11,17 +11,6 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 class FozzyGraphQLBundle extends Bundle
 {
     public function build(ContainerBuilder $container){
-
-//        $container->registerForAutoconfiguration(GraphqlQueryInterface::class)
-//            ->addTag(GraphqlQueryInterface::TAG_MAME)
-//        ;
-//
-//        $container->registerForAutoconfiguration(GraphqlMutationInterface::class)
-//            ->addTag(GraphqlMutationInterface::TAG_MAME)
-//        ;
-
-
-
         parent::build($container);
         $container
             ->addCompilerPass(new GraphQLCompilerPath(), PassConfig::TYPE_BEFORE_OPTIMIZATION, 5)

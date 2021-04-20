e50608de3a0972faa9f8b10454393aa7872e135b
olexiy <o.chuiko@temabit.com>
2021-04-20 12:07:00 +0300

    change bundle name

--- DependencyInjection/OzznestGraphQLExtension.php
+++ DependencyInjection/OzznestGraphQLExtension.php
@@ -9,7 +9,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
-class GraphQLExtension  extends Extension
+class OzznestGraphQLExtension  extends Extension
 {
     public function load(array $configs, ContainerBuilder $container){
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
